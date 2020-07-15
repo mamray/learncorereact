5c0b04fe7480ab64c42dcbe52e2a36e8b080291d
Victor Mamray <mamray@gmail.com>
2020-07-15 18:53:15 +0300

    Using reorg

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Domain;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
+using Domain;
 using Persistence;
 
 namespace DatingApp.API.Controllers
